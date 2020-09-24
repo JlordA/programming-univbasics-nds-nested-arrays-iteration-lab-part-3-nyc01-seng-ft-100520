5b0281fe64bb08a2b74e7ba24a4f1b23cf98bedf
placid-tier-4550 <james.ardery@gmail.com>
2020-09-24 14:39:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   long_strings = []
   row_index = 0
   while row_index < src.count do
-    element_index = nil
+    element_index = 0
     element_string = []
     while element_index < src[row_index].count do
       #binding.pry
